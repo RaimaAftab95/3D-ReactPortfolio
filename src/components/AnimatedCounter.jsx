c339534fe87933c0d2c2fc6ca0c079197b959128
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-05-06 20:37:57 +0500

    Counter is now working on all screens and see my work btn goto counter section

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -12,33 +12,6 @@ const AnimatedCounter = () => {
   const counterRef = useRef(null);
   const countersRef = useRef([]);
 
-  //   useGSAP(() => {
-  //     countersRef.current.forEach((counter, index) => {
-  //       const numberElement = counter.querySelector(".counter-number");
-  //       const item = counterItems[index];
-
-  //       // Set initial value to 0
-  //       gsap.set(numberElement, { innerText: "0" });
-
-  //       // Create the counting animation
-  //       gsap.to(numberElement, {
-  //         innerText: item.value,
-  //         duration: 2.5,
-  //         ease: "power2.out",
-  //         snap: { innerText: 1 }, // Ensures whole numbers
-  //         scrollTrigger: {
-  //           trigger: "#counter",
-  //           start: "top center",
-  //           markers: true,
-  //         },
-  //         // Add the suffix after counting is complete
-  //         onComplete: () => {
-  //           numberElement.textContent = `${item.value}${item.suffix}`;
-  //         },
-  //       });
-  //     }, counterRef);
-  //   }, []);
-
   useGSAP(() => {
     countersRef.current.forEach((counter, index) => {
       const numberElement = counter.querySelector(".counter-number");
