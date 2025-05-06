c0b7776e61bdb1970f4b4e5e430113ef7329aa92
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-05-06 20:35:34 +0500

    Add counter

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -18,8 +18,11 @@ const Button = ({ text, className, id }) => {
           const offset = window.innerHeight * 0.15; // Leave a bit of space at the top
 
           // Calculate how far down the page we need to scroll
+          // const top =
+          //   target.getBoundingClientRect().top + window.pageYOffset - offset;
+
           const top =
-            target.getBoundingClientRect().top + window.pageYOffset - offset;
+            target.getBoundingClientRect().top + window.scrollY - offset;
 
           // Scroll smoothly to that position
           window.scrollTo({ top, behavior: "smooth" });
